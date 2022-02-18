986bf7267de4560d1ec6f092a53669bbfd5b9393
Jonah Kornberg <kornberg.jonah@gmail.com>
2022-02-18 10:53:36 -0500

    hide image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,25 @@
 
-import { Container, Flex, VStack, Button, Divider, Text, Center, HStack, Image} from '@chakra-ui/react'
+import { Container, Flex, VStack, Button, Divider, Text, Center, HStack, Image } from '@chakra-ui/react'
 import Head from 'next/head';
 
 const Home = () => (
   <div>
     <Head>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js"/>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js" />
     </Head>
     <Container maxW="container.xl" p={0}>
-        <VStack w="full" h="full" p={10} alignItems="start">
-            <Button colorScheme={'blue'} size='lg' borderRadius='3xl' alignSelf={'center'} m='20'>Upload 3D File (STL, STEP)</Button>
-          <Text>History</Text>
-          <Divider bgColor = 'black'/>
-          <HStack h='50px' w='50px' bgColor={'gray.500'}><Image></Image></HStack>
-        </VStack>
+      <VStack w="full" h="full" p={10} alignItems="start">
+        <Button colorScheme={'blue'} size='lg' borderRadius='3xl' alignSelf={'center'} m='20'>Upload 3D File (STL, STEP)</Button>
+        <Text>History</Text>
+        <Divider bgColor='black' />
+        <HStack h='50px' w='50px' bgColor={'gray.500'}>
+          {/* <Image></Image> */}
+        </HStack>
+      </VStack>
 
     </Container>
-    </div>
-  );
+  </div>
+);
 
 
 export default Home;
